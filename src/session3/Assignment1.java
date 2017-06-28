ea2fc8c68e3da93a1c46d732f22235ffa5158360
Sudhir.Nayak <Sudhir.Nayak@WLW7-SUDHIRN.INDECOMM.LOCAL>
2017-06-28 08:02:20 +0530

    Session3 Assignment1

---
+++ src/session3/Assignment1.java
@@ -0,0 +1,16 @@
+package session3;
+
+public class Assignment1 {
+	static{
+		print(10);
+	}
+	static void print (int x) {
+		System.out.println (x);
+		System.exit(0);
+	}
+	public static void main(String args[]){
+	
+
+}
+
+}
